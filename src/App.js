a3956a7f0410a33c438b2c6792b3c42e30a92ba6
ngoctan1234 <ngoctannew@gmail.com>
2024-03-16 19:26:49 +0700

    ud1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Chỉ là giấc mơ</h1>
+        </div>
+    );
+}
+
+export default App;
